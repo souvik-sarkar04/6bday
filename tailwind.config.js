99659735ad66c49dd74db4a52e7d65bb2e30b379
souvik-sarkar04 <148918451+souvik-sarkar04@users.noreply.github.com>
2024-11-06 00:31:52 +0530

    commit 1

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    './src/**/*.{html,js}', // Adjust this path based on your project structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
